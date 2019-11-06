3fed9eca335e277ef242f1bc1192205fb7b3563c
ZijunZhao98 <zijun_zhao1998@163.com>
2019-11-06 14:51:49 -0600

    changed a lot and added banner

--- router.js
+++ router.js
@@ -4,15 +4,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render("home", {css: "home.css"});
+  res.render("projects");
 });
 
 router.get('/resume', function(req, res){
   res.render("resume");
 })
 
-router.get('/projects', function(req, res){
-  res.render("projects");
+router.get('/profile', function(req, res){
+  res.render("profile");
 });
 
 module.exports = router;
