57666b7a2706b4cdb40ca2c3a4fd01a7937f387c
ZijunZhao98 <zijun_zhao1998@163.com>
2019-11-08 11:11:44 -0600

    added link

--- public/js/projects.js
+++ public/js/projects.js
@@ -15,4 +15,32 @@ $('document').ready(function(){
     $( location ).attr("href", "#games");
   });
 
+  $('#usocial').on('click', function(evt){
+    window.open("https://github.com/ZijunZhao98/USocial");
+  });
+
+  $('#slack_bot').on('click', function(evt){
+    window.open("https://github.com/amyible/slackbot");
+  });
+
+  $('#lifegame').on('click', function(evt){
+    window.open("https://github.com/ZijunZhao98/Game");
+  });
+
+  $('#wish').on('click', function(evt){
+    window.open("https://github.com/danhuiZ/wishes");
+  });
+
+  $('#WashU_combat').on('click', function(evt){
+    window.open("https://github.com/ZijunZhao98/Moltgey");
+  });
+
+  $('#calendar').on('click', function(evt){
+    window.open("https://github.com/ZijunZhao98/online_calendar");
+  });
+
+  $('#richArduino').on('click', function(evt){
+    window.open("https://github.com/SaundersJP/RichArduino");
+  });
+
 });
